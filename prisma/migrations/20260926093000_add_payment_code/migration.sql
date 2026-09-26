b490b632bb4baf30a0c5dcb8568286a1c40712dc
herbatium-gif <herbatium@gmail.com>
2026-09-26 10:18:02 +0300

    Update migration.sql

--- prisma/migrations/20260926093000_add_payment_code/migration.sql
+++ prisma/migrations/20260926093000_add_payment_code/migration.sql
@@ -1,1 +1,8 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "paymentCode" TEXT,
+ADD COLUMN     "declaredMonths" INTEGER,
+ADD COLUMN     "declaredAmountRon" INTEGER,
+ADD COLUMN     "declaredAt" TIMESTAMP(3);
 
+-- CreateIndex
+CREATE UNIQUE INDEX "User_paymentCode_key" ON "User"("paymentCode");
